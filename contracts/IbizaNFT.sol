7b05ece115827493f20255409cead5e5cf643091
fabiopak <fabio.pacchioni@gmail.com>
2021-06-08 20:26:01 +0200

    Add documentation

--- contracts/IbizaNFT.sol
+++ contracts/IbizaNFT.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.1;
 
-//import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
@@ -11,12 +9,14 @@ import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
 contract IbizaNFT is Context, AccessControlEnumerable, ERC721Enumerable, ERC721Burnable, ERC721Pausable {
     using Strings for uint256;
     using Counters for Counters.Counter;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant URI_SETTER_ROLE = keccak256('URI_SETTER_ROLE');
 
     Counters.Counter private _tokenIdTracker;
 
@@ -24,10 +24,6 @@ contract IbizaNFT is Context, AccessControlEnumerable, ERC721Enumerable, ERC721B
 
     // Optional mapping for token URIs
     mapping (uint256 => string) private _tokenURIs;
-
-    // bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
-    bytes32 public constant URI_SETTER_ROLE = keccak256('URI_SETTER_ROLE');
-    // bytes4 private constant _INTERFACE_ID_FEES = 0xb7799584;
 
     constructor(string memory name, string memory symbol, string memory baseTokenURI) ERC721(name, symbol) {
         _baseTokenURI = baseTokenURI;
@@ -39,12 +35,19 @@ contract IbizaNFT is Context, AccessControlEnumerable, ERC721Enumerable, ERC721B
         _setupRole(URI_SETTER_ROLE, _msgSender());
     }
 
+    /**
+    * @dev internal function to get URI base
+    */
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
-    function setURIBase(string calldata uriBase_) public onlyRole(URI_SETTER_ROLE) {
-        _baseTokenURI = uriBase_;
+    /**
+    * @dev set URI base (URI_SETTER_ROLE only)
+    * @param _uriBase string for the URI base address
+    */
+    function setURIBase(string calldata _uriBase) public onlyRole(URI_SETTER_ROLE) {
+        _baseTokenURI = _uriBase;
     }
 
     /**
@@ -65,7 +68,9 @@ contract IbizaNFT is Context, AccessControlEnumerable, ERC721Enumerable, ERC721B
     }
 */
     /**
-     * @dev See {IERC721Metadata-tokenURI}.
+     * @dev See {IERC721Metadata-tokenURI}. Read tokenURI in a tokenID
+     * @param tokenId token ID
+     * @return token URI string
      */
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "IbizaNFT: URI query for nonexistent token");
@@ -86,17 +91,21 @@ contract IbizaNFT is Context, AccessControlEnumerable, ERC721Enumerable, ERC721B
     }
 
     /**
-     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
-     *
-     * Requirements:
-     *
-     * - `tokenId` must exist.
-     */
+    * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+    * Requirements:
+    * - `tokenId` must exist.
+    * @param tokenId token ID
+    * @param _tokenURI token URI string
+    */
     function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
         require(_exists(tokenId), "IbizaNFT: URI set of nonexistent token");
         _tokenURIs[tokenId] = _tokenURI;
     }
 
+    /**
+    * @dev mint a token based on current enumeration and sends it to a recipient, with tokenURI formed by baseURI + counter
+    * @param to recipient address
+    */
     function mint(address to) public virtual {
         require(hasRole(MINTER_ROLE, _msgSender()), "IbizaNFT: must have minter role to mint");
 
@@ -106,27 +115,47 @@ contract IbizaNFT is Context, AccessControlEnumerable, ERC721Enumerable, ERC721B
         _tokenIdTracker.increment();
     }
 
+    /**
+    * @dev mint a token based on a custom id and sends it to a recipient, with tokenURI formed by baseURI + counter
+    * @param to recipient address
+    * @param tokenId custom token id
+    */
     function mintTokenID(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) {
         _mint(to, tokenId);
         _tokenIdTracker.increment();
     }
 
-    function mintComplete(address recipient, uint256 tokenId, string memory _TokenURI) public onlyRole(MINTER_ROLE) {
-        _mint(recipient, tokenId);
-        _setTokenURI(tokenId, _TokenURI);
+    /**
+    * @dev mint a token based on a custom id and sends it to a recipient, with tokenURI formed by baseURI + custo token URI
+    * @param to recipient address
+    * @param tokenId custom token id
+    * @param _tokenURI custom token URI to add to baseURI
+    */
+    function mintComplete(address to, uint256 tokenId, string memory _tokenURI) public onlyRole(MINTER_ROLE) {
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
         _tokenIdTracker.increment();
     }
 
+    /**
+    * @dev pause the contract
+    */
     function pause() public virtual {
         require(hasRole(PAUSER_ROLE, _msgSender()), "IbizaNFT: must have pauser role to pause");
         _pause();
     }
 
+    /**
+    * @dev unpause the contract
+    */
     function unpause() public virtual {
         require(hasRole(PAUSER_ROLE, _msgSender()), "IbizaNFT: must have pauser role to unpause");
         _unpause();
     }
 
+    /**
+    * @dev function called before any token transfer
+    */
     function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
